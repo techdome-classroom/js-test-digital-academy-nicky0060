1146030ab2f83e9fe3a1f63433b1ca6096041811
nikita <nikitagawande006@gmail.com>
2024-02-15 15:02:57 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -7,12 +7,12 @@ var romanToInt = function(s) {
     const romanvalue =
  { 
     I :1,
-    V 5,
-    X 10,
-    L 50,
-    C 100,
-    D 500,
-    M 1000 
+    V :5,
+    X :10,
+    L :50,
+    C :100,
+    D :500,
+    M :1000 
  }
 
 };
